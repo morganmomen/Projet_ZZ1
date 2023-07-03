e3704acf698196587ab57c3a06251b2762751a75
morganmomen <morganmomen.pro@gmail.com>
2023-07-03 15:30:39 +0200

    Fonction recuit simulé

--- recuit_simule/recuit_simule.c
+++ recuit_simule/recuit_simule.c
@@ -1,24 +1,18 @@
 #include "recuit_simule.h"
-#include "../rules/changeRules.h"
-#include "../rules/rules.h"
-#include "../rules/energy.h"
+#include "rules/changeRules.h"
+#include "rules/energy.h"
+#include "rules/rules.h"
 
-
-
-
-
-
-// rule_t ** recuit_simule (rule_t **rules,int T, int energy(int** grapheComplet, rule_t **state, int N)) 
-// {
-//     regle_t ** newrules = malloc(NB_RULES*sizeof(rules));
-//     while (T > 0) 
-//     {
-//         genereEtat(grapheComplet, state, newState, N);
-//         if (calculPoids(grapheComplet,newState,N) < calculPoids(grapheComplet,state,N)) 
-//         if (calculPoids(grapheComplet,newState,N) > state->poids) state=newState;
-//         double p = exp((state->poids - calculPoids(grapheComplet,newState,N)) / T);
-//         if (p > rand() / RAND_MAX) state=newState;
-//         T--;
-//     }
-//     return state;
-// }
+ruleSet_t *recuit_simule(ruleSet_t *rules, int T, int energy(ruleSet_t *rules)) {
+  ruleSet_t *newRules;
+  while (T > 0) {
+    newRules = changeRuleSet(rules);
+    if (energy(newRules) < energy(rules))
+      rules = newRules;
+    double p = exp((energy(rules) - energy(newRules)) / T);
+    if (p > rand() / RAND_MAX)
+      rules = newRules;
+    T--;
+  }
+  return rules;
+}
