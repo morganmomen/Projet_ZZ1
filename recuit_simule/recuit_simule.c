3853fa59cd9e54bf5339dd0c7cf43e5016eb56b9
morganmomen <morganmomen.pro@gmail.com>
2023-07-03 15:39:33 +0200

    Calcul de l'énergie du nouveau ruleset au préalable +rajout de la nouvelle energie

--- recuit_simule/recuit_simule.c
+++ recuit_simule/recuit_simule.c
@@ -3,16 +3,21 @@
 #include "rules/energy.h"
 #include "rules/rules.h"
 
-ruleSet_t *recuit_simule(ruleSet_t *rules, int T, int energy(ruleSet_t *rules)) {
+ruleSet_t *recuit_simule(ruleSet_t *rules, int T,
+                         int energy(ruleSet_t *rules)) {
   ruleSet_t *newRules;
   while (T > 0) {
     newRules = changeRuleSet(rules);
-    if (energy(newRules) < energy(rules))
+    newRules->energy = energy(newRules);
+    if (newRules->energy < rules->energy)
       rules = newRules;
+    rules->energy = energy(newRules);
     double p = exp((energy(rules) - energy(newRules)) / T);
     if (p > rand() / RAND_MAX)
       rules = newRules;
+    rules->energy = energy(newRules);
     T--;
   }
+
   return rules;
 }
