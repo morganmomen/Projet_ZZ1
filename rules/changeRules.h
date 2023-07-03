7c0c6ff6955d95a28fc12d18cf421e6800f65799
morganmomen <morganmomen.pro@gmail.com>
2023-07-03 15:14:57 +0200

    Fichier d'en tete changeRules

--- rules/changeRules.h
+++ rules/changeRules.h
@@ -1,1 +1,8 @@
 #include "rules.h"
+case_t change_case(case_t *_case, int direction, int changement);
+direction4_t change_direction(direction4_t *direction, int changement);
+distance_t change_distance(distance_t *distance, int changement);
+action_t change_action(action_t *action, int changement);
+int change_priority(int priority, int changement);
+rule_t *changeRule(ruleSet_t rules, int id_rule);
+ruleSet_t *changeRuleSet(ruleSet_t *rules);
--- rules/energy.c
+++ rules/energy.c
@@ -1,2 +1,2 @@
 #include "energy.h"
-
+#include "rules.h"
