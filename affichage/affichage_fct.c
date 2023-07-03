5fa9e7abca40a6f1d6c1c9ba760e182ad5905790
PierreBourdiaux <pierre.bourdiaux@etu.uca.fr>
2023-07-03 19:14:45 +0200

    maj details mineur

--- affichage/affichage_fct.c
+++ affichage/affichage_fct.c
@@ -1,25 +1,35 @@
 #include "affichage_fct.h"
 
+
+ int window_w=-1, window_h=-1;
+ SDL_Texture* texture[3] = {NULL,NULL,NULL};
+
+
+
 void affichefond(SDL_Renderer * renderer,SDL_Window * window ,int** map)
 {
     
-    SDL_DisplayMode screen;
-    SDL_GetCurrentDisplayMode(0, &screen);
+    
 
-    int window_w, window_h;
+    if(window_w==-1 || window_h==-1){
+        SDL_DisplayMode screen;
+        SDL_GetCurrentDisplayMode(0, &screen);
+        SDL_GetWindowSize(window, &window_w, &window_h);
 
-    SDL_GetWindowSize(window, &window_w, &window_h);
+    }
+
+
 
     int offsetX =   window_w /(N+2);
     int offsetY = window_h/(N+2);
     printf("offsetX = %d\n",offsetX);
     printf("offsetY = %d\n",offsetY);
 
-    SDL_Texture* texture[3];
-    texture[0] = loadTexture("../img/gazon.png", renderer);
-    texture[1] = loadTexture("../img/brique.png", renderer);
-    texture[2] = loadTexture("../img/terrier.png", renderer);
-
+    if(texture[0] == NULL||texture[1] == NULL||texture[2] == NULL){
+        texture[0] = loadTexture("../img/gazon.png", renderer);
+        texture[1] = loadTexture("../img/brique.png", renderer);
+        texture[2] = loadTexture("../img/terrier.png", renderer);
+    }
 
 
     for(int i =0; i<N+2; i++){
