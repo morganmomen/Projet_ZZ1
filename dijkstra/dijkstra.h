d2c22cd840559e36d0cc8bf47a6b5644474b1d36
pibonnet10 <pierre.bonnet@etu.uca.fr>
2023-07-04 10:26:03 +0200

    a jour

--- dijkstra/dijkstra.h
+++ dijkstra/dijkstra.h
@@ -1,4 +1,17 @@
-typedef struct {
-    int x;
-    int y;
-} position;
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef __DIJKSTRA__H
+#define __DIJKSTRA__H
+
+
+
+int **transform_for_dijktra(int **mat, int taille) ;
+int trouverMinDistance(int distance[], int visite[], int numSommet);
+int * dijkstra(int ** graphe, int depart, int arrivee, int taille);
+void afficherChemin(int  parent [], int sommet);
+int* obtenirChemin(int predecesseurs[], int sommet, int* tailleChemin);
+
+#endif
