c2b55b52afd41b0646740e3823cc9c332d9ca67c
pibonnet10 <pierre.bonnet@etu.uca.fr>
2023-07-06 11:45:14 +0200

    jeu directionnel

--- gestion_jeu/gestion_jeu.h
+++ gestion_jeu/gestion_jeu.h
@@ -23,6 +23,7 @@
 int lancer_jeu_sans_graphisme (int taille, int * nb_iterations);
 
 int lancer_jeu_avec_graphisme (int taille, int *nb_iterations);
+int lancer_jeu_avec_graphisme_avec_control_lapin (int taille, int *nb_iterations);
 
 
 #endif
--- gestion_jeu/gestion_jeu_main.c
+++ gestion_jeu/gestion_jeu_main.c
@@ -41,38 +41,55 @@ int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
-    int taille = 15;
+    int taille = 50;
 
-    /*SANS GRAPHISME*/
-    printf("Bienvenue dans le jeu du chasseur et du lapin\n");
-    int nbLancer = 100;
-    int resultat[nbLancer];
-    int nb_iterations[nbLancer];
-    for(int i = 0; i < nbLancer; i++)
+    /*Avec direction fleches*/
+
+    int nbiterations;
+    int resultat = lancer_jeu_avec_graphisme_avec_control_lapin(taille, &nbiterations);
+    if(resultat == 0)
     {
-        resultat[i] = lancer_jeu_sans_graphisme(taille, &nb_iterations[i]);
-        printf("Iteration %d\n", i);
+        printf("Le chasseur a gagné\n");
     }
-
-    int nb_victoire_chasseur = 0;
-    int nb_victoire_lapin = 0;
-    int nb_iterations_total = 0;
-    for(int i = 0; i < nbLancer; i++)
+    else
     {
-        if(resultat[i] == 0)
-        {
-            nb_victoire_chasseur++;
-        }
-        else
-        {
-            nb_victoire_lapin++;
-        }
-        nb_iterations_total += nb_iterations[i];
+        printf("Le lapin a gagné\n");
     }
+    
+    printf("Le nombre d'itérations est de %d\n", nbiterations);
+    
 
-    printf("Le chasseur a gagné %d fois\n", nb_victoire_chasseur);
-    printf("Le lapin a gagné %d fois\n", nb_victoire_lapin);
-    printf("Le nombre moyen d'itérations est de %d\n", nb_iterations_total/nbLancer);
+
+    // /*SANS GRAPHISME*/
+    // printf("Bienvenue dans le jeu du chasseur et du lapin\n");
+    // int nbLancer = 100;
+    // int resultat[nbLancer];
+    // int nb_iterations[nbLancer];
+    // for(int i = 0; i < nbLancer; i++)
+    // {
+    //     resultat[i] = lancer_jeu_sans_graphisme(taille, &nb_iterations[i]);
+    //     printf("Iteration %d\n", i);
+    // }
+
+    // int nb_victoire_chasseur = 0;
+    // int nb_victoire_lapin = 0;
+    // int nb_iterations_total = 0;
+    // for(int i = 0; i < nbLancer; i++)
+    // {
+    //     if(resultat[i] == 0)
+    //     {
+    //         nb_victoire_chasseur++;
+    //     }
+    //     else
+    //     {
+    //         nb_victoire_lapin++;
+    //     }
+    //     nb_iterations_total += nb_iterations[i];
+    // }
+
+    // printf("Le chasseur a gagné %d fois\n", nb_victoire_chasseur);
+    // printf("Le lapin a gagné %d fois\n", nb_victoire_lapin);
+    // printf("Le nombre moyen d'itérations est de %d\n", nb_iterations_total/nbLancer);
 
     /*AVEC GRAPHISME*/
     // int nbiterations;
@@ -137,6 +154,7 @@ int main(int argc, char **argv)
     // }
 
     printf("FIN DES THREADS\n");
+    return 0;
 }
 
 
